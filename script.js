ba180baf0aca0df244efdfa7113da019e23d9a97
KarthikaMovva <145451949+KarthikaMovva@users.noreply.github.com>
2023-11-22 09:35:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//Challenge 1: Store the user's first name, last name, age, country, and state in your browser's localStorage using the setItem() method and print the localStorage in the console.
+localStorage.setItem("first name","Sravani")
+localStorage.setItem("last name","Dasari")
+localStorage.setItem("age","25")
+localStorage.setItem("country","India")
+localStorage.setItem("state","Andhra Pradesh")
+console.log(localStorage)
+//Challenge 2: Get the users data stored on your browser's localStorage using the getItem() method and print the localStorage in the console.
+localStorage.getItem("first name")
+localStorage.getItem("last name")
+localStorage.getItem("age")
+localStorage.getItem("country")
+localStorage.getItem("state")
+console.log(localStorage)
+//Challenge 3: Using removeItem() method, remove the "state" data stored in your browser's localStorage and print the localStorage in the console.
+localStorage.removeItem("state")
+console.log(localStorage)
+//Challenge 4: Using clear() method to remove all the data stored in your browser's localStorage and print the localStorage in the console.
+localStorage.clear()
+console.log(localStorage)
+//Challenge 5: Using JSON.stringify() convert the given object into the string and print the same.
+const user = {
+  firstName: 'Rajat',
+  age: 25,
+  skills: ['HTML', 'CSS', 'JS', 'React'],
+};
+var convert = JSON.stringify(user)
+console.log(user)
